6fb556b614d15da4926948563b17de100df021c5
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2019-11-11 18:47:28 +0000

    menu component complete

--- src/menu.component.js
+++ src/menu.component.js
@@ -4,19 +4,30 @@ import './menu.less';
 export const MenuComponent = {
     template: `
         <ul>
-            <li menu-item="node" ng-repeat="node in $ctrl.nodes">
-                {{ node.title }}
-                <menu ng-if="node.nodes" nodes="node.nodes" />
+            <li ng-repeat="node in $ctrl.nodes">
+                <button type="button" ng-click="$ctrl.onClick(node)">
+                    {{ node.title }} <span ng-if="node.nodes">-></span>
+                </button>
+                <menu ng-show="node.collapsed" ng-if="node.nodes" nodes="node.nodes" />
             </li>
         </ul>
     `,    
 
     controller: class MenuComponent {
+        $onInit() {
+            this.collapsed = true;
+        }
+
         $onChanges(changes) {
             if (changes.nodes) {
                 this.nodes = [...this.nodes];
                 debug(`nodes changed => ${JSON.stringify(this.nodes)}`);
             }
+        }
+
+        onClick(node) {
+            debug(`node => "${node.title}" clicked`);
+            node.collapsed = !node.collapsed;
         }
     },
 
--- src/menu.model.js
+++ src/menu.model.js
@@ -4,8 +4,8 @@ export const menu = [
         title: "A Root level",
         nodes: [
             {title: "First level 1", nodes: [
-                {title: "A level 2"},
-                {title: "A level 2"},
+                {title: "A1 level 2"},
+                {title: "A2 level 2"},
             ]},
             {title: "Third level 1"},
         ]
@@ -15,8 +15,8 @@ export const menu = [
         nodes: [
             {title: "Second level 1"},
             {title: "Four level 1", nodes: [
-                {title: "B level 2"},
-                {title: "B level 2"},
+                {title: "B1 level 2"},
+                {title: "B2 level 2"},
             ]},
         ]
     },
