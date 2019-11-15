4b6fd90b36f1b9bc2698dab1a3fa070bf79fae09
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2019-11-15 18:42:49 +0000

    make menu component as stateless

--- src/app.component.js
+++ src/app.component.js
@@ -7,7 +7,8 @@ export const AppComponent = {
     template: `
         <div>
             <h2>Hello, Word!</h2>
-            <menu type="toolbar" nodes="$ctrl.nodes" />
+            <menu type="toolbar" nodes="$ctrl.nodes" on-click="$ctrl.onClick($event)"
+                  on-collapse="$ctrl.onCollapse($event)"/>
         </div>
     `,
     controller: class AppComponent {
@@ -24,6 +25,14 @@ export const AppComponent = {
         $postLink() {
             const {$element} = this;
             debug(`AppComponent#postLink:: $element => ${$element}`);
+        }
+
+        onClick({node}) {
+            info(`onClick => "${node.title}"`);
+        }
+
+        onCollapse({node}) {
+            node.collapsed = !node.collapsed;
         }
     },
 };
--- src/menu.component.js
+++ src/menu.component.js
@@ -4,15 +4,16 @@ import './menu.less';
 export const MenuComponent = {
     template: `
         <li ng-repeat="node in $ctrl.nodes">
-            <button type="button" ng-click="$ctrl.onClick(node)">
+            <button type="button" ng-click="$ctrl.onClick({$event:{node}})">
                 {{ node.title }}
             </button>
-            <button type="button" ng-if="node.nodes" ng-click="$ctrl.onClickArrow(node)">
+            <button type="button" ng-if="node.nodes" ng-click="$ctrl.onCollapse({$event:{node}})">
                 <svg viewBox="0 0 10 10">
                     <path d="M7 9L5 8 3 9V6L1 4h3l1-3 1 3h3L7 6z"/>
                 </svg>
             </button>
-            <menu ng-show="node.collapsed" ng-if="node.nodes" type="toolbar" nodes="node.nodes" />
+            <menu ng-show="node.collapsed" ng-if="node.nodes" type="toolbar" nodes="node.nodes"
+                  on-click="$ctrl.onClick({$event})" on-collapse="$ctrl.onCollapse({$event})"/>
         </li>
     `,    
 
@@ -31,20 +32,13 @@ export const MenuComponent = {
 
         $postLink() {
             const {$element} = this;
-            debug(`MenuComponent#postLink:: $element => ${$element}`);
             $element.attr('nodes-count', this.nodes.length);
-        }
-
-        onClick(node) {
-            info(`node => "${node.title}" clicked`);
-        }
-
-        onClickArrow(node) {
-            node.collapsed = !node.collapsed;
         }
     },
 
     bindings: {
         nodes: '<',
+        onClick: '&',
+        onCollapse: '&',
     },
 };
