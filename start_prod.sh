2f9664148a29709069661a200c03cfaa169b398f
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-03-17 19:40:23 +0000

    start pruduction docker image script

--- start_prod.sh
+++ start_prod.sh
@@ -4,6 +4,7 @@
 
 APP_VERSION=`node -pe "require('./package.json').version"`
 LOCAL_PORT=127.0.0.1:8080
+LOCAL_PORT_SSL=127.0.0.1:8443
 CONTAINER_NAME=${APP_NAME}.prod_v${APP_VERSION}
 
 if [ -z "$1" ]; then
@@ -20,4 +21,4 @@ if [ -n "${CONTAINER_ID}" ]; then
 fi
 
 echo "start production image => ${PRODUCTION_IMAGE_NAME}"
-docker run --rm -d -p ${LOCAL_PORT}:80/tcp --name ${CONTAINER_NAME} ${PRODUCTION_IMAGE_NAME}
+docker run --rm -d -p ${LOCAL_PORT}:80/tcp -p ${LOCAL_PORT_SSL}:443/tcp --name ${CONTAINER_NAME} ${PRODUCTION_IMAGE_NAME}
