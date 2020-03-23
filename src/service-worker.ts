5eabd9216f0267df526cd24cb2545beb2334bd45
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-03-23 19:39:42 +0000

    cache strategies in service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,4 +1,10 @@
 import { debug } from './logger';
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst, StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+
+registerRoute(/\.(js|json)$/, new NetworkFirst());
+registerRoute(/\.css$/, new StaleWhileRevalidate({ cacheName: 'css-cache' }));
+registerRoute(/\.(?:png|jpg|jpeg|svg|gif)$/, new CacheFirst({ cacheName: 'image-cache' }));
 
 const sw: ServiceWorkerGlobalScope = self as any;
 
@@ -11,12 +17,3 @@ sw.addEventListener('install', (event: Event) => {
 sw.addEventListener('activate', (event: ExtendableEvent) => {
     debug('sw:: activate event =>', event);
 });
-
-sw.addEventListener('fetch', (event: FetchEvent) => {
-    const {request: {method, url}} = event;
-    debug(`sw:: fetch event => ${method} ${url}`);
-});
-
-const {serviceWorker: {state}, caches} = sw;
-
-debug('sw:: state => ', state, ', caches => ', caches);
